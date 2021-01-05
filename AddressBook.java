aef45772f077ebd3fe14ef98b1dd41885d3ceea1
Sandeep Singh <sandeepgaurdec13@gmail.com>
2021-01-05 19:47:54 +0530

    Added:Edit detail of the employee on the basis on first name

--- AddressBook.java
+++ AddressBook.java
@@ -1,53 +1,75 @@
+
 /**
  * @author Sandeep_Singh 
- * functionality Add More Address
+ * functionality print Welcome Message
  *
  */
-import java.util.ArrayList;
-import java.util.Scanner;
+public class AddressBook{
 
-public class AddressBook 
-{
-	public static ArrayList<Person> contacts = new ArrayList<Person>();
-
-	private static void addContact() 
-	{
-		Scanner inp = new Scanner(System.in);
-                private static void addContact()
+private static void addContact()
+		String zipCode = inp.nextLine();
+		System.out.println("Enter the email-id:");
+		String email = inp.nextLine();
 		Address personAddress = new Address(city,state,zipCode,email);
 		Person details = new Person(firstName, lastName, phoneNum, personAddress);
+		Person details = new Person(firstName, lastName, phoneNum, city,  state, zipCode, email);
 		System.out.println(details.toString());
-	}		
 		contacts.add(details);
 	}	
+	private static void edit() 
+	{
+		Scanner inp = new Scanner(System.in); 
+		System.out.println("Enter firstname to update details");
+		String name = inp.nextLine();
+		for (Person person : contacts) 
+		{
+			if (name.equals(person.getFirstName())) 
+			{
+				Scanner newInput = new Scanner(System.in);
+				System.out.println("Enter the LastName:");
+				String lastName = newInput.nextLine();
+				System.out.println("Enter the phone:");
+				String phoneNum = newInput.nextLine();
+				System.out.println("\nAddress:");
+				System.out.println("Enter the city:");
+				String city = newInput.nextLine();
+				System.out.println("Enter the state:");
+				String state = newInput.nextLine();
+				System.out.println("Enter the zipcode:");
+				String zipCode = newInput.nextLine();
+				System.out.println("Enter the email-id:");
+				String email = newInput.nextLine();
+				person.setLastName(lastName);
+				person.setphoneNumber(phoneNum);
+				person.setCity(city);
+				person.setState(state);
+				person.setZipCode(zipCode);
+				person.setEmail(email);
+			}
+		}	
+	}
 	private static void display() 
 	{
 		for (Person person : contacts) {
-			System.out.println(person.toString());
-		}
-	}
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to Address Book Program");
-		addContact();
-		int cond = 1;
-		while (cond == 1) 
-		{
 			System.out.println("Address Book:\n"
 					+ "1) Add Person\n"
 					+ "2) Display\n"
+					+ "3) Edit\n"
 					+ "0) Close");
 			Scanner inp = new Scanner(System.in);
+			Scanner option = new Scanner(System.in);
 			System.out.println("Select an option:");
 			int num=inp.nextInt();
+			int num=option.nextInt();
 			switch (num) 
 			{
 			case 1:
-				addContact();
-				break;
+ public static void main(String[] args)
 			case 2:
 				display();
+				break;
+			case 3:
+				edit();
 				break;
 			case 0:
 				cond=0;
@@ -57,6 +79,7 @@ public class AddressBook
 				break;
 			}
 		}
-
 	}
+}
+	
 }
